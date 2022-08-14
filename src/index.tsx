3f1fe2496dd51f77281990db679aa80b659d267c
Lucas Phang <lucasphang98@gmail.com>
2022-08-14 15:29:36 +1000

    feat: init near app on start and refresh

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./index.css";
 import App from "./pages/App";
 import reportWebVitals from "./reportWebVitals";
+import { Account, Contract, Near, WalletConnection } from "near-api-js";
+
+import { initContract } from "./util";
 
 import * as buffer from "buffer";
 
@@ -11,18 +14,32 @@ import * as buffer from "buffer";
 // https://github.com/near/near-api-js/issues/757
 (window as any).Buffer = buffer.Buffer;
 
+declare global {
+  interface Window {
+    near: Near;
+    walletConnection: WalletConnection;
+    accountId: string;
+    account: Account;
+    contract: Contract;
+  }
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+initContract()
+  .then(() => {
+    root.render(
+      <React.StrictMode>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+          </Routes>
+        </BrowserRouter>
+      </React.StrictMode>
+    );
+  })
+  .catch(console.error);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
